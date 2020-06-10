cf31ea278958e30226bb775a508cb0baf9b49e9f
Richard Walker <richard.walker.90@gmail.com>
2020-06-10 17:51:10 -0400

    Lesson 25, Conditional Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { forbiddenNameValidator } from './shared/username.validator';
 import { passwordValidator } from './shared/password.validator';
 
@@ -8,26 +8,49 @@ import { passwordValidator } from './shared/password.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  registrationForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group({
+      userName: ['Richie', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/admin/)]],
+      email: [''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+      address: this.formBuilder.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      })
+    },
+      { validator: passwordValidator }
+    )
+
+    this.registrationForm.get('subscribe').valueChanges.subscribe(checkedValue => {
+      const email = this.registrationForm.get('email');
+      
+      if (checkedValue) {
+        email.setValidators(Validators.required);
+      } else {
+        email.clearValidators();
+      }
+
+      email.updateValueAndValidity();
+    })
+
+  }
 
   get userName() {
     return this.registrationForm.get('userName');
   }
 
-  registrationForm = this.formBuilder.group({
-    userName: ['Richie', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/admin/)]],
-    password: [''],
-    confirmPassword: [''],
-    address: this.formBuilder.group({
-      city: [''],
-      state: [''],
-      postalCode: ['']
-    })
-  },
-    { validator: passwordValidator }
-  )
+  get email() {
+    return this.registrationForm.get('email');
+  }
 
   public loadAllApiData() {
     // If you use the setValue function, 
