f63d26f137e7d99ec03180702da120103d740d50
Richard Walker <richard.walker.90@gmail.com>
2020-06-16 16:29:54 -0400

    Lesson 26 & 27, Dynamic Forms and Submission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, FormArray } from '@angular/forms';
 import { forbiddenNameValidator } from './shared/username.validator';
 import { passwordValidator } from './shared/password.validator';
+import { RegistrationService } from './registration.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,9 @@ export class AppComponent implements OnInit {
 
   registrationForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private registrationService: RegistrationService) { }
 
   ngOnInit(): void {
     this.registrationForm = this.formBuilder.group({
@@ -25,14 +29,13 @@ export class AppComponent implements OnInit {
         city: [''],
         state: [''],
         postalCode: ['']
-      })
-    },
-      { validator: passwordValidator }
-    )
+      }),
+      alternateEmails: this.formBuilder.array([])
+    }, { validator: passwordValidator })
 
     this.registrationForm.get('subscribe').valueChanges.subscribe(checkedValue => {
       const email = this.registrationForm.get('email');
-      
+
       if (checkedValue) {
         email.setValidators(Validators.required);
       } else {
@@ -52,19 +55,29 @@ export class AppComponent implements OnInit {
     return this.registrationForm.get('email');
   }
 
+  get alternateEmails() {
+    return this.registrationForm.get('alternateEmails') as FormArray;
+  }
+
+  addAlternateEmail() {
+    this.alternateEmails.push(this.formBuilder.control(''));
+  }
+
   public loadAllApiData() {
     // If you use the setValue function, 
     // you HAVE to pass ALL the FormGroups and FormControl values
     this.registrationForm.setValue({
       userName: 'Sasha',
+      email: 'richie@gmail.com',
       password: 'baby',
       confirmPassword: 'baby',
-
+      subscribe: false,
       address: {
         city: 'Kingston',
         state: 'NC',
         postalCode: '12345'
-      }
+      },
+      alternateEmails: []
     })
   }
 
@@ -76,6 +89,16 @@ export class AppComponent implements OnInit {
       password: 'bar',
       confirmPassword: 'bar'
     })
+  }
+
+  public onSubmit() {
+    console.log(this.registrationForm.value);
+    this.registrationService
+      .register(this.registrationForm.value)
+      .subscribe(
+        response => console.log('Success'),
+        error => console.error('Error')
+      );
   }
 
 }
